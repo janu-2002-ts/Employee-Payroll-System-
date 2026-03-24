c26b531561400d4b43ddf3dad3263a2497b0adf5
janu-2002-ts <147641506+janu-2002-ts@users.noreply.github.com>
2026-03-24 14:04:19 +0530

    Add Employee class with basic attributes and methods

---
+++ Employee.java
@@ -0,0 +1,36 @@
+public class Employee 
+{
+    private int id;
+    private String name;
+    private double basic;
+
+    public int getId() 
+    {
+        return id;
+    }
+
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public double getBasic() 
+    {
+        return basic;
+    }
+
+    public void setBasic(double basic) 
+    {
+        this.basic = basic;
+    }
+}
